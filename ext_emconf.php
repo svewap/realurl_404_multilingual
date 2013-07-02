c4c8621b3d4fa125e09be632c93c659ebaa14705
Sven Wappler <info@wapplersystems.de>
2013-07-02 19:41:38 +0200

    version number updated

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,21 +3,21 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "realurl_404_multilingual".
  *
- * Auto generated 14-01-2013 01:26
+ * Auto generated 13-03-2013 23:01
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'RealURL 404 multilingual error page',
 	'description' => 'Shows the defined error page of the given language if the requested page or file could not be found',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.0.0',
-	'dependencies' => '',
-	'conflicts' => '',
+	'version' => '1.0.1',
+	'dependencies' => 'realurl',
+	'conflicts' => 'error_404_handling',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
@@ -30,24 +30,23 @@ $EM_CONF[$_EXTKEY] = array (
 	'author' => 'Sven Wappler',
 	'author_email' => 'typo3YYYY@wapplersystems.de',
 	'author_company' => '',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
 			'realurl' => '',
 			'php' => '5.0.0-0.0.0',
 			'typo3' => '6.0.0-0.0.0',
 		),
-		'conflicts' => 
-		array (
+		'conflicts' => array(
 			'error_404_handling' => '',
 		),
-		'suggests' => 
-		array (
+		'suggests' => array(
 		),
 	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:4:{s:12:"ext_icon.gif";s:4:"789d";s:17:"ext_localconf.php";s:4:"4d38";s:30:"Classes/Hooks/FrontendHook.php";s:4:"ac32";s:14:"doc/manual.sxw";s:4:"839f";}',
 );
 
 ?>
