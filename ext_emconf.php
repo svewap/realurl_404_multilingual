be01c16d13877cbe36c90f23dc4806af54c39a93
Alex Bachynskyi <alex@pixelant.se>
2017-04-13 18:06:08 +0300

    [TASK] adjust composer.json to typo3 8.7 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'realurl' => '',
-			'typo3' => '6.0.0-7.9.99',
+			'typo3' => '6.0.0-8.9.99',
 		),
 		'conflicts' => array(
 			'error_404_handling' => '',
